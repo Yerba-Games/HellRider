7a255bbb02875b0b3ea1a04f100e0be3a7cb0351
DarkTuck <konstantytadeusz@gmail.com>
2022-12-06 22:32:36 +0100

    added movement and First Person Camera
    fuck me
    I hate Cinemachine
    fuck the phisic

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using NaughtyAttributes;
+public class PlayerMovment : MonoBehaviour
+{
+    Actions input;
+    [SerializeField] [Foldout("Settings")] float Speed, Jump;
+    [SerializeField] [Foldout("Settings")] Camera playerCamera;
+    private float inputX, inputZ;
+    private Rigidbody rb;
+    private InputAction move;
+    private Vector3 forceDirection = Vector3.zero;
+    private void OnEnable()
+    {
+        input.Player.Jump.started += DoJump;
+        move = input.Player.Move;
+        input.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Player.Jump.started -= DoJump;
+        input.Player.Move.Disable();
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        input = new Actions();
+
+    }
+
+    private void FixedUpdate()
+    {
+        forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(playerCamera) * Speed;
+        forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(playerCamera) * Speed;
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
+
+        if (rb.velocity.y < 0f)
+            rb.velocity -= Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
+
+        Vector3 horizontalVelocity = rb.velocity;
+        horizontalVelocity.y = 0;
+        if (horizontalVelocity.sqrMagnitude > Speed * Speed)
+            rb.velocity = horizontalVelocity.normalized * Speed + Vector3.up * rb.velocity.y;
+        LookAt();
+    }
+    private void DoJump(InputAction.CallbackContext obj)
+    {
+        if (IsGrounded())
+        {
+            forceDirection += Vector3.up * Jump;
+        }
+    }
+    private Vector3 GetCameraForward(Camera playerCamera)
+    {
+        Vector3 forward = playerCamera.transform.forward;
+        forward.y = 0;
+        return forward.normalized;
+    }
+    private void LookAt()
+    {
+        Vector3 direction = rb.velocity;
+        direction.y = 0f;
+
+        if (move.ReadValue<Vector2>().sqrMagnitude > 0.1f && direction.sqrMagnitude > 0.1f)
+        {
+            this.rb.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+        else
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private Vector3 GetCameraRight(Camera playerCamera)
+    {
+        Vector3 right = playerCamera.transform.right;
+        right.y = 0;
+        return right.normalized;
+    }
+    private bool IsGrounded()
+    {
+        Ray ray = new Ray(this.transform.position - (Vector3.up * 0.25f), Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1f))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
